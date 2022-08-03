4070ce59ad5d24b7afb60c941fa99538f1a824a8
Shivansh Kaushal <kaushal.shivansh630@gmail.com>
2022-08-03 13:58:20 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wafer-fault-detection",
+    version="0.0.3",
+    description="ML project",
+    author="Shivansh Kaushal", 
+    packages=find_packages()
+)
